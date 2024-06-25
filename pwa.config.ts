3fe49a40e06cbfe088d2246c82ac982dc03c1c77
CodesWithSubham <149304598+CodesWithSubham@users.noreply.github.com>
2024-06-25 21:48:08 +0530

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -10,31 +10,31 @@ const pwaConfig: Config = {
   backgroundColor: '#fff',
   themeColor: '#fff',
   display: 'standalone',
-  
+  orientation: 'natural',
   scope: '/',
   startUrl: '/?utm_source=homescreen',
   appleStatusBarStyle: 'black-translucent',
   preferRelatedApplications: false,
   shortcuts: [
     {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
+      name: 'Anime Girls Photos',
+      shortName: 'Anime Girls Photos',
+      description: 'Anime Girls Photos',
+      url: '/search/label/Girl?utm_source=homescreen',
     },
     {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
+      name: 'Anime Girls Photos',
+      shortName: 'Anime Girls Photos',
+      description: 'Anime Girls Photos',
+      url: '/search/label/Boy?utm_source=homescreen',
     },
   ],
   pwa: {
     logs: true,
     // OneSignal is not available if you are not using cloudflare workers
-    oneSignalEnabled: false,
+    oneSignalEnabled: true,
     oneSignalConfig: {
-      appId: '********-****-****-****-************',
+      appId: 'fc603d92-e2e7-4f48-b981-a108cd507001',
       allowLocalhostAsSecureOrigin: true,
     },
   },
